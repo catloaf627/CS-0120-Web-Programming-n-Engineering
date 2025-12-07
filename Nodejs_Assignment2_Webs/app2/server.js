ed2544059244405e048d1c681d4a08428d70c073
Yuqing <wangyuqing.rachel@gmail.com>
2025-12-06 23:23:14 -0500

    assignment10

--- Nodejs_Assignment2_Webs/app2/server.js
+++ Nodejs_Assignment2_Webs/app2/server.js
@@ -46,4 +46,9 @@ app.post("/process", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
